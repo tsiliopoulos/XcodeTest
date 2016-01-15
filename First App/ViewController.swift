200bf9f9df1eeb5aaadb81f610c3023937e16d5f
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-01-15 00:34:31 -0500

    Initial Commit

---
+++ First App/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  First App
+//
+//  Created by Tom Tsiliopoulos on 2016-01-14.
+//  Copyright © 2016 Tom Tsiliopoulos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //some output
+        print("hello")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
